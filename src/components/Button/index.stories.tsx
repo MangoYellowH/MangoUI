dd1867e52ba8470ad7e4cf59150926e4c8ae93e3
huangyuan3h <huangyuan3h@gmail.com>
2020-04-04 22:35:34 +0800

    update the story

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -1,15 +1,19 @@
 import React, { ReactElement } from 'react';
 import { text } from '@storybook/addon-knobs';
+import Mango from '../../HOC/WithTheme/Mango';
+import Skyline from '../../HOC/WithTheme/Skyline';
 import Button from './index';
 
 export default {
   title: 'components/button',
 };
 
-const buttonText = text('text', 'button text');
-
-export const ButtonStory = (): ReactElement => (
+const ButtonStory = (): ReactElement => (
   <Button onClick={() => 0}>
-    {buttonText}
+    {text('text', 'button text')}
   </Button>
 );
+
+export const mango = Mango<{}>(ButtonStory);
+
+export const skyline = Skyline<{}>(ButtonStory);
