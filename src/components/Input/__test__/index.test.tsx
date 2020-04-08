6982bac3842ef2c2e0ddb5f3f5d42226538f4c8c
huangyuan3h <huangyuan3h@gmail.com>
2020-04-09 00:57:33 +0800

    update test case

--- src/components/Input/__test__/index.test.tsx
+++ src/components/Input/__test__/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import Input from '..';
 
 describe('Input Component', () => {
@@ -7,15 +7,27 @@ describe('Input Component', () => {
   test('test input could input value and return the value by on change', () => {
     const inputClass = 'InputClass';
     const inputValue = 'mockValue';
-    const mockFn = jest.fn();
-    const { getByText } = render(
+    const inputValueChanged = 'mockValueChanged';
+    const inputPlaceHolder = 'mockPlaceHolder';
+    const mockFn = jest.fn((val: string): void => {
+      expect(val).toBe(inputValueChanged);
+    });
+    const { queryByPlaceholderText } = render(
       <Input
         classes={inputClass}
         onChange={mockFn}
         value={inputValue}
+        placeholder={inputPlaceHolder}
       />,
     );
-    expect(getByText).not.toBeNull();
-    // todo: add test
+    const element = queryByPlaceholderText(inputPlaceHolder) as HTMLInputElement;
+
+    expect(element).toHaveClass(inputClass);
+
+    expect((element.value)).toBe(inputValue);
+
+    fireEvent.change(element, { target: { value: inputValueChanged } });
+
+    expect(mockFn).toBeCalledTimes(1);
   });
 });
